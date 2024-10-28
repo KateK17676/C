46345195bd3d6c7ecd2f0cf8656358937c434a7d
Андрей Боев <a.boev.it@gmail.com>
2024-10-28 15:36:24 +0300

    1. Введение

--- Введение/Введение.c
+++ Введение/Введение.c
@@ -1,7 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <locale.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[])
 {
-    printf("");
+    setlocale(0, "RU");
+    // Даны стороны прямоугольника width и height. Необходимо найти его площадь и периметр.
+    float width, height;
+    scanf("%f", &width);
+    scanf("%f", &height);
+
+    printf("Площадь: %f\n", width * height);
+    printf("Периметр: %f", 2 * (width + height));
     return 0;
 }
